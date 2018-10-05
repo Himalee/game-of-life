4af7eb401eedefd38d04192d8bb367aafd25c710
Himalee <himalee.tailor@gmail.com>
2018-10-05 14:35:17 +0100

    Test & add: Cell class

---
+++ lib/cell.rb
@@ -0,0 +1,43 @@
+class Cell
+
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def get_live_neighbours(coordinate)
+    neighbours = []
+    get_neighbour_coordinates(coordinate).each do |coordinate|
+      if coordinate_inside_grid?(coordinate)
+        neighbours << @grid.grid[coordinate[0]][coordinate[1]]
+      end
+    end
+    count_live_cells(neighbours)
+  end
+
+  private
+
+  def count_live_cells(neighbours)
+    neighbours.flatten.count(1)
+  end
+
+  def get_neighbour_coordinates(coordinate)
+    x = coordinate[0]
+    y = coordinate[1]
+
+    neighbours = []
+    neighbours << [x - 1, y + 1]
+    neighbours << [x, y + 1]
+    neighbours << [x + 1, y + 1]
+    neighbours << [x - 1, y]
+    neighbours << [x + 1, y]
+    neighbours << [x - 1, y - 1]
+    neighbours << [x, y - 1]
+    neighbours << [x + 1, y - 1]
+
+    neighbours
+  end
+
+  def coordinate_inside_grid?(coordinate)
+    coordinate[0] >= 0 && coordinate[1] >= 0
+  end
+end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,22 @@
+require 'cell'
+require 'grid'
+
+describe Cell do
+  let :grid { Grid.new([[0, 1, 0], [1, 0, 1], [0, 1, 0]]) }
+
+  it "returns 0 when every cell around it is empty" do
+    empty_grid = Grid.new([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    cell = Cell.new(empty_grid)
+    expect(cell.get_live_neighbours([1, 1])).to eq 0
+  end
+
+  it "returns number of living neighbours given grid and coordinate" do
+    cell = Cell.new(grid)
+    expect(cell.get_live_neighbours([1, 1])).to eq 4
+  end
+
+  it "returns number of living neighbours given grid and outer coordinate" do
+    cell = Cell.new(grid)
+    expect(cell.get_live_neighbours([0, 0])).to eq 2
+  end
+end
