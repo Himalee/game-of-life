da5609b35f3accebf483ded1d6ce7367c4aacfb2
Himalee <himalee.tailor@gmail.com>
2018-10-05 15:45:24 +0100

    Test: valid neighbour coordinates

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,8 +5,8 @@ describe Cell do
   let :grid { Grid.new([[0, 1, 0], [1, 0, 1], [0, 1, 0]]) }
 
   it "returns 0 when every cell around it is empty" do
-    empty_grid = Grid.new([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
-    cell = Cell.new(empty_grid)
+    almost_empty_grid = Grid.new([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    cell = Cell.new(almost_empty_grid)
     expect(cell.get_live_neighbours([1, 1])).to eq 0
   end
 
@@ -18,5 +18,11 @@ describe Cell do
   it "returns number of living neighbours given grid and outer coordinate" do
     cell = Cell.new(grid)
     expect(cell.get_live_neighbours([0, 0])).to eq 2
+  end
+
+  it "returns number of living neighbours given grid and another outer coordinate" do
+    almost_empty_grid = Grid.new([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    cell = Cell.new(almost_empty_grid)
+    expect(cell.get_live_neighbours([0, 0])).to eq 1
   end
 end
